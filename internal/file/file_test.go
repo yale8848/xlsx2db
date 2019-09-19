385eaa301905851c8e33a0d413bb59ce5360b0b0
yale8848 <royal8848@163.com>
2019-09-19 18:18:00 +0800

    delete demos

--- internal/file/file_test.go
+++ internal/file/file_test.go
@@ -5,11 +5,11 @@ import "testing"
 
 func TestXlsx_GetDataByIndex(t *testing.T) {
 
-	xf := NewFile()
-	p := `C:\Users\Yale\Desktop\易学卡分成和保底分成合作单位明细(1).xlsx`
-	xf.GetDataByIndex(p, []int{1}, func(i int, strings map[int]string) error {
+	/*	xf := NewFile()
+		p := ""
+		xf.GetDataByIndex(p, []int{1}, func(i int, strings map[int]string) error {
 
-		return nil
-	})
+			return nil
+		})*/
 
 }
